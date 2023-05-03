bede1ae9a001490f2a5a944c90e3b7da2517eae7
ricardofavale <88509917+ricardofavale@users.noreply.github.com>
2023-05-03 10:04:32 -0300

    Atualizando para Typescript
    Atualizando o projeto para Typescript

---
+++ src/components/TagsPrato/index.tsx
@@ -0,0 +1,24 @@
+import classNames from 'classnames';
+import styles from './TagsPrato.module.scss';
+import { Prato } from 'types/Prato';
+
+export default function TagsPrato({
+  category,
+  size, 
+  serving,
+  price
+}: Prato) {
+  return (
+    <>
+      <div className={styles.tags}>
+        <div className={classNames({
+          [styles.tipo]: true,
+          [styles[`tags__tipo__${category.label.toLowerCase()}`]]: true
+        })}>{category.label}</div>
+        <div className={styles.tags__porcao}>{size}g</div>
+        <div className={styles.tags__qtdpessoas}>{serving} 2 pessoa{serving === 1 ? '' : 's'}</div>
+        <div className={styles.tags__valor}>R$ {price.toFixed(2)}</div>
+      </div>
+    </>
+  );
+}
